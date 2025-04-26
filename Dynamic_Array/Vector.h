89318aee3b05aef6c0c9548479966a0fcc4fded3
Ryan Lam <mrsssprite@gmail.com>
2025-04-26 16:34:02 +0800

    completed dynamic array

--- Dynamic_Array/Vector.h
+++ Dynamic_Array/Vector.h
@@ -1,4 +1,33 @@
 #ifndef __VECTOR_H
 #define __VECTOR_H
 
+/*----------------------------- Public Includes ------------------------------*/
+#include <stddef.h>
+/*--------------------------- Public Includes END ----------------------------*/
+
+/*------------------------------ Public Defines ------------------------------*/
+#define vector_init(elem_type) vector_init_elem_sz(sizeof(elem_type))
+/*---------------------------- Public Defines END ----------------------------*/
+
+/*----------------------------- Public Typedefs ------------------------------*/
+typedef struct vector *Vector;
+/*--------------------------- Public Typedefs END ----------------------------*/
+
+/*------------------------ Public Function Prototypes ------------------------*/
+struct vector *vector_init_elem_sz(size_t elem_sz);
+void vector_destroy(struct vector *this);
+
+size_t vector_size(struct vector *this);
+size_t vector_capacity(struct vector *this);
+
+int vector_push(struct vector *this, void *item);
+int vector_pop(struct vector *this);
+int vector_insert(struct vector *this, void *restrict dest,
+                  void *restrict first, size_t n);
+int vector_erase(struct vector *this, void *first, size_t n);
+
+void *vector_begin(struct vector *this);
+void *vector_end(struct vector *this);
+/*---------------------- Public Function Prototypes END ----------------------*/
+
 #endif
